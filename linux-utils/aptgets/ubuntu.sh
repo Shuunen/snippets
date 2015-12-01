60052c14b8a32b85f852e314bb58b9a2268e475d
Romain Racamier Lafon <romain.racamier@gmail.com>
2015-12-01 01:29:40 +0100

    $1

--- linux-utils/aptgets/ubuntu.sh
+++ linux-utils/aptgets/ubuntu.sh
@@ -6,4 +6,5 @@ sudo apt-get install nano htop ncdu pydf -y
 sudo apt-get install git -y
 git config --global user.email "romain.racamier@gmail.com"
 git config --global user.name "Romain Racamier Lafon"
-
+git config --global push.default simple
+git config --global credential.helper cache
