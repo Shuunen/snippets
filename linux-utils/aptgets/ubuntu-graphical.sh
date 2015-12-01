b7661fb0533a03dacdcb1e62ca69adc747d0808f
Romain Racamier Lafon <romain.racamier@gmail.com>
2015-12-01 22:44:18 +0100

    added dconf

--- linux-utils/aptgets/ubuntu-graphical.sh
+++ linux-utils/aptgets/ubuntu-graphical.sh
@@ -7,3 +7,6 @@ sudo apt-get install nautilus-open-terminal -y
 
 # gedit complete
 sudo apt-get install gedit gedit-developer-plugins gedit-plugins gedit-source-code-browser-plugin -y
+
+# handy debian conf
+sudo apt-get install dconf-editor -y
