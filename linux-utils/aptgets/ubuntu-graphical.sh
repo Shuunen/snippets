4a106d951d4a3f0a1c6a4ccb86227ffc7252168c
Romain Racamier Lafon <romain.racamier@gmail.com>
2015-12-01 01:52:08 +0100

    $1

--- linux-utils/aptgets/ubuntu-graphical.sh
+++ linux-utils/aptgets/ubuntu-graphical.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #
 # Usage: chmod +x ./ubuntu-graphical.sh && ./ubuntu-graphical.sh
-sudo apt-get nautilus-open-terminal
+
+# nautilus plugins
+sudo apt-get install nautilus-open-terminal -y
+
+# gedit complete
+sudo apt-get install gedit gedit-developer-plugins gedit-plugins gedit-source-code-browser-plugin -y
--- linux-utils/aptgets/ubuntu.sh
+++ linux-utils/aptgets/ubuntu.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
 # Usage: chmod +x ./ubuntu.sh && ./ubuntu.sh
+
+# mini softs
 sudo apt-get install nano htop ncdu pydf -y
 
+# git
 sudo apt-get install git -y
 git config --global user.email "romain.racamier@gmail.com"
 git config --global user.name "Romain Racamier Lafon"
 git config --global push.default simple
 git config --global credential.helper cache
+
