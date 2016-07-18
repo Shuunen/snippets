dc27210790eaf081081548dccc23e907588f547e
Romain Racamier Lafon <romain.racamier@gmail.com>
2016-07-18 21:51:08 +0200

    added comments & handled dynamic filtering on successful ajax requests

--- bookmarklets/feedly-filter.js
+++ bookmarklets/feedly-filter.js
@@ -1,11 +1,16 @@
+// the list of word(s) to exclude
+// this list is overridden when window.avoid is defined earlier
 var avoid = avoid ? avoid : "Dark Vador, Pokemon Go";
+// to lower to avoid being case sensitive
 avoid = avoid.toLowerCase().replace(/\s*,\s*/g, '|');
 console.info('will remove rows with one of these words');
 console.info(avoid.replace(/\|/g, ' | '));
 var escapeRegExp = function (str) {
     return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$]/g, '\\$&');
 };
+// clean up accents in strings
 var accentsTidy = function (inputStr) {
+    // to lower to avoid being case sensitive
     var str = inputStr.toLowerCase();
     var notGood = {
         a: '[àáâãäå]',
@@ -27,23 +32,49 @@ var accentsTidy = function (inputStr) {
 var uniqueValues = function (value, index, self) {
     return self.indexOf(value) === index;
 };
+// a simple forEach function to be cross browser
 var forEach = function (array, callback, scope) {
     for (var i = 0; i < array.length; i++) {
         callback.call(scope, i, array[i]);
     }
 };
-var elements = document.querySelectorAll('.entryList .title');
-forEach(elements, function (index, element) {
-    var str = accentsTidy(element.text);
-    str = str.replace(/[\W_0-9]+/g, ' ').trim();
-    var match = str.match(new RegExp(escapeRegExp(avoid), 'g'));
-    if (match) {
-        match = match.filter(uniqueValues);
-        if (match[0]) {
-            console.warn(index + ' - detected "' + match.join(', ') + '" in title %c' + str, "color:black;font-style: bold");
-            element.parentElement.parentElement.firstElementChild.lastElementChild.click();
+// the main job of this script
+var markExcludedAsRead = function () {
+    var feeds = document.querySelectorAll('.entryList .title');
+    forEach(feeds, function (index, feed) {
+        // to lower to avoid being case sensitive + remove accents
+        var str = accentsTidy(feed.text);
+        // remove non letter/numeric characters & underscores
+        str = str.replace(/[\W_]+/g, ' ').trim();
+        var match = str.match(new RegExp(escapeRegExp(avoid), 'g'));
+        if (match) {
+            match = match.filter(uniqueValues);
+            if (match[0]) {
+                console.warn(index + ' - detected "' + match.join(', ') + '" in title %c' + str, "color:black;font-style: bold");
+                // will click on mark as read & hide button on that feed
+                feed.parentElement.parentElement.firstElementChild.lastElementChild.click();
+            }
+        } else {
+            console.log(index + ' - nothing in title %c' + str, "color:darkgrey;font-style: italic");
         }
-    } else {
-        console.log(index + ' - nothing in title %c' + str, "color:darkgrey;font-style: italic");
+    });
+};
+// will filter feeds after a successful ajax request
+// pretty useful when changing feeds category
+var listenForAjaxRequest = function (callback) {
+    _send = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function () {
+        var original = this.onreadystatechange;
+        this.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                // delay a bit the callback execution
+                setTimeout(callback, 200);
+            }
+            original.apply(this, arguments);
+        };
+        _send.apply(this, arguments);
     }
-});
+};
+// init stack
+markExcludedAsRead();
+listenForAjaxRequest(markExcludedAsRead);
