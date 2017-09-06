7cbd7d70a731d396e8895b129b2f2dd74527174d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-09-06 14:04:55 +0200

    even simpler api auth

--- api-auth/index.js
+++ api-auth/index.js
@@ -1,33 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const basicAuth = require('basic-auth')
-
-const auth = function (req, res, next) {
-    function unauthorized(res) {
-        res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
-        return res.send(401)
-    }
-
-    const user = basicAuth(req)
-
-    if (!user || !user.name || !user.pass) {
-        return unauthorized(res)
-    }
-
-    if (user.name === 'foo' && user.pass === 'bar') {
-        return next()
-    } else {
-        return unauthorized(res)
-    }
-}
+const basicAuth = require('express-basic-auth')
 
 app.use(cors())
 
-app.get('/', auth, function (req, res) {
-    res.status(200).send('Authenticated :)')
-})
+app.use(basicAuth({ users: { 'foo': 'bar' }, challenge: true }))
 
-app.listen(3000, function () {
-    console.log('Api Auth listening on port 3000')
-})
+app.get('/', (req, res) => res.status(200).send('If you see this, you\'re authenticated :)'))
+
+app.listen(3000, () => console.log('Api with basic auth listening on port 3000'))
