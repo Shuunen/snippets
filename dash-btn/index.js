a3367819547f100345c626aa0a4a166f52d582de
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-12-10 13:41:38 +0100

    added red btn

--- dash-btn/index.js
+++ dash-btn/index.js
@@ -3,9 +3,13 @@ const dash_button = require('node-dash-button')
 
 const buttons = [
     {
-        name: 'signal',
+        name: 'white',
         mac: 'fc:a6:67:8f:42:7c'
-    }
+    },
+    {
+        name: 'red',
+        mac: '34:d2:70:18:10:cc'
+    },
 ]
 
 buttons.forEach(button => {
