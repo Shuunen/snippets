434fed4cf9d44d70e223ccbf87b31f2d7ab32cb0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-12-10 13:15:22 +0100

    nicer generic listeners creator

--- dash-btn/index.js
+++ dash-btn/index.js
@@ -1,11 +1,19 @@
-const macs = {
-  signal: 'fc:a6:67:8f:42:7c'
+
+const dash_button = require('node-dash-button')
+
+const buttons = [
+    {
+        name: 'signal',
+        mac: 'fc:a6:67:8f:42:7c'
+    }
+]
+
+buttons.forEach(button => {
+    console.log(`listening to button "${button.name}" with mac ${button.mac}`)
+    button.instance = dash_button(button.mac, null, null, 'arp')
+    button.instance.on('detected', onClick(button))
+})
+
+function onClick(button) {
+    console.log(`"${button.name}" has been clicked`)
 }
-
-var dash_button = require('node-dash-button');
-
-var dash = dash_button(macs.signal, null, null, 'arp');
-
-dash.on("detected", function (){
-  console.log("omg found signal :)");
-});
