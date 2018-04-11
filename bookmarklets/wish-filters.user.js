0f57366922d295b6cb8f357e87171ffdd251a004
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-04-11 22:30:00 +0200

    optimized script

--- bookmarklets/wish-filters.user.js
+++ bookmarklets/wish-filters.user.js
@@ -11,6 +11,12 @@
 $(document).ready(function() {
   console.log('wish price filter : init');
 
+  var minPrice = 0;
+  var maxPrice = 1000;
+  var minStars = 1;
+  var hideFree = false;
+  var hideNsfw = true;
+
   // Returns a function, that, as long as it continues to be invoked, will not
   // be triggered. The function will be called after it stops being called for
   // N milliseconds. If `immediate` is passed, trigger the function on the
@@ -31,64 +37,63 @@ $(document).ready(function() {
     };
   }
 
-  function fetchData(id, productEl, delay, noRecursion) {
+  function fetchData(id, productEl, originalPicture) {
     console.log('will get data for', id);
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const url = 'https://www.wish.com/c/' + id;
-        console.log('getting data for', id);
-        fetch(url)
-          .then((r) => r.text())
-          .then((html) => {
-            const dataMatches = html.match(/"aggregateRating" : ([\w\W\n]+"\n}),/gi);
-            const dataStr = dataMatches[0];
-            const data = JSON.parse('{' + dataStr.replace('},', '}').replace(/\n/g, '') + '}');
-            const ratings = Math.round(data.aggregateRating.ratingValue * 100) / 100;
-            const count = Math.round(data.aggregateRating.ratingCount);
-            console.log(id, ': found a rating of', ratings, 'over', count, 'reviews :)');
-            let roundedRatings = Math.round(ratings);
-            let ratingsStr = '';
-            while (roundedRatings--) {
-              ratingsStr += '<img class="abw-star" src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-20.png" />';
-            }
-            if (count > 0) {
-              ratingsStr += '<hr> over ' + count + ' reviews';
-            } else {
-              ratingsStr = 'no reviews !';
-            }
-            productEl.find('.feed-details-row2').css('display', 'flex').css('align-items', 'center').html(ratingsStr);
-            const shippingMatches = html.match(/"localized_shipping":\s{"localized_value":\s(\d)/i);
-            const shippingFees = parseInt(shippingMatches[1]);
-            console.log(id, ': shipping fees', shippingFees, '€');
-            const priceMatches = productEl.find('.feed-actual-price').text().match(/\d+/);
-            const price = parseInt(priceMatches && priceMatches.length ? priceMatches[0] : 0);
-            console.log(id, ': base price', price, '€');
-            const totalPrice = shippingFees + price + ' €';
-            productEl.find('.feed-actual-price').html(totalPrice);
-            const nsfwMatches = html.match(/sex|lingerie|crotch|masturbat|vibrator|bdsm|bondage|nipple/gi);
-            if (nsfwMatches && nsfwMatches.length) {
-              productEl.addClass('abw-nsfw');
-            }
-            showHideProductsDebounced(noRecursion);
-            resolve({ id, ratings, count, shippingFees, price, totalPrice });
-          })
-          .catch((error) => {
-            console.error('did not managed to found ratings for product "', id, '"', error);
-          });
-      }, delay);
-    });
+    const url = 'https://www.wish.com/c/' + id;
+    console.log('getting data for', id);
+    fetch(url)
+      .then((r) => r.text())
+      .then((html) => {
+        const dataMatches = html.match(/"aggregateRating" : ([\w\W\n]+"\n}),/gi);
+        const dataStr = dataMatches[0];
+        const data = JSON.parse('{' + dataStr.replace('},', '}').replace(/\n/g, '') + '}');
+        const ratings = Math.round(data.aggregateRating.ratingValue * 100) / 100;
+        const count = Math.round(data.aggregateRating.ratingCount);
+        // console.log(id, ': found a rating of', ratings, 'over', count, 'reviews :)');
+        let roundedRatings = Math.round(ratings);
+        let ratingsStr = '';
+        while (roundedRatings--) {
+          ratingsStr += '<img class="abw-star" src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-20.png" />';
+        }
+        if (count > 0) {
+          ratingsStr += '<hr> over ' + count + ' reviews';
+        } else {
+          ratingsStr = 'no reviews !';
+        }
+        productEl.find('.feed-details-row2').css('display', 'flex').css('align-items', 'center').html(ratingsStr);
+        const shippingMatches = html.match(/"localized_shipping":\s{"localized_value":\s(\d)/i);
+        const shippingFees = parseInt(shippingMatches[1]);
+        // console.log(id, ': shipping fees', shippingFees, '€');
+        const priceMatches = productEl.find('.feed-actual-price').text().match(/\d+/);
+        const price = parseInt(priceMatches && priceMatches.length ? priceMatches[0] : 0);
+        // console.log(id, ': base price', price, '€');
+        const totalPrice = shippingFees + price + ' €';
+        productEl.find('.feed-actual-price').html(totalPrice);
+        const nsfwMatches = html.match(/sex|lingerie|crotch|masturbat|vibrator|bdsm|bondage|nipple/gi);
+        if (nsfwMatches && nsfwMatches.length) {
+          productEl.addClass('abw-nsfw');
+        }
+        showHideProduct(productEl, originalPicture);
+      })
+      .catch((error) => {
+        console.error('did not managed to found ratings for product "', id, '"', error);
+      });
   }
 
   var loadedUrl = '//main.cdn.wish.com/fd9acde14ab5/img/ajax_loader_16.gif?v=13';
 
-  function getData(element, noRecursion) {
+  function getData(element) {
     const productEl = $(element.tagName ? element : element.currentTarget);
     if (productEl.hasClass('abw-with-data')) {
-      console.log('product has already data');
+      // console.log('product has already data');
       return;
     }
     productEl.addClass('abw-with-data');
     const image = productEl.find('a.display-pic');
+    if (!image || !image[0]) {
+      console.error('did not found image on product', productEl);
+      return;
+    }
     var href = image.attr('href');
     if (!href) {
       console.error('did not found href on product', productEl);
@@ -98,71 +103,77 @@ $(document).ready(function() {
     const originalPicture = image[0].style.backgroundImage;
     image[0].style.backgroundImage = 'url(' + loadedUrl + ')';
     image[0].style.backgroundSize = '10%';
-    fetchData(id, productEl, 200, noRecursion).then(() => {
-      image[0].style.backgroundImage = originalPicture;
-      image[0].style.backgroundSize = '100%';
-    });
+    fetchData(id, productEl, originalPicture);
   }
 
   function getNextData() {
     const amount = 10;
     console.log('getting next', amount, 'items data');
-    $('.feed-product-item:not(.abw-with-data):lt(' + amount + ')').each((index, element) => {
-      setTimeout(() => getData(element, true), index * 300);
+    $('.feed-product-item:visible:not(.abw-with-data):lt(' + amount + ')').each((index, element) => {
+      setTimeout(() => getData(element), index * 300);
     });
   }
-  // debounced version
-  var getNextDataDebounced = debounce(getNextData, 2000);
+
+  function showHideProduct(element, originalPicture) {
+    const productEl = $(element);
+    const priceEl = productEl.find('.feed-actual-price');
+    var price = priceEl.text().replace(/\D/g, '');
+    var nbStars = productEl.find('img.abw-star').size();
+    var priceOk = price <= maxPrice;
+    if (minPrice && minPrice > 0) {
+      priceOk = priceOk && price >= minPrice;
+    }
+    if (priceOk && hideFree && priceEl[0].textContent.includes('Free')) {
+      priceOk = false;
+    }
+    if (priceOk && minStars && minStars > 0 && productEl.hasClass('abw-with-data')) {
+      priceOk = nbStars >= minStars;
+    }
+    if (priceOk && hideNsfw && productEl.hasClass('abw-nsfw')) {
+      priceOk = false;
+    }
+    if (priceOk) {
+      productEl.show('fast');
+      if (originalPicture) {
+        const image = productEl.find('a.display-pic');
+        if (!image || !image[0]) {
+          console.error('did not found image on product', productEl);
+          return;
+        }
+        image[0].style.backgroundImage = originalPicture;
+        image[0].style.backgroundSize = '100%';
+      }
+      if (!productEl.hasClass('abw-on-hover')) {
+        productEl.addClass('abw-on-hover');
+        productEl.hover(getData);
+      }
+    } else {
+      productEl.hide('fast');
+    }
+  }
 
   function showHideProducts(event) {
+    console.log('wish price filter : showHideProducts');
+    setTimeout(hideUseless, 100);
+    setTimeout(getNextData, 100);
+    $('.feed-product-item').each((index, element) => {
+      showHideProduct(element);
+    });
+  }
+
+  // prepare a debounced function
+  var showHideProductsDebounced = debounce(showHideProducts, 1000);
+
+  // activate when window is scrolled
+  window.onscroll = showHideProductsDebounced;
+
+  function hideUseless() {
     // hide already rated products in order hsitory
     $('.edit-rating-button').parents('.transaction-expanded-row-item').hide();
     // hide products that can't be rated
     $('.late-box').parents('.transaction-expanded-row-item').hide();
     // delete useless marketing stuff
     $('.discount-banner, .urgency-inventory, .feed-crossed-price, .product-boost-rect, .header-hello, .badge-details, .faster-shipping-wrapper').remove();
-    console.log('wish price filter : showHideProducts');
-    if (event && event.type === 'scroll') {
-      getNextData();
-    }
-    var minPrice = parseInt($('#wtc_min_price').val()) || 0;
-    var maxPrice = parseInt($('#wtc_max_price').val()) || 1000;
-    var minStars = parseInt($('#wtc_min_stars').val()) || 0;
-    var hideFree = $('#wtc_hide_free').is(':checked');
-    var hideNsfw = $('#wtc_hide_nsfw').is(':checked');
-    localStorage.abwHideFree = hideFree;
-    localStorage.abwHideNsfw = hideNsfw;
-    localStorage.abwMinStars = minStars;
-    // console.log('wish price filter : hide free items ?', hideFree);
-    var items = $('.feed-actual-price');
-    $.each(items, function() {
-      var product = $(this).parent().parent().parent().parent();
-      var price = $(this).text().replace(/\D/g, '');
-      var nbStars = product.find('img.abw-star').size();
-      var priceOk = price <= maxPrice;
-      if (minPrice && minPrice > 0) {
-        priceOk = priceOk && price >= minPrice;
-      }
-      if (priceOk && hideFree && this.textContent.includes('Free')) {
-        priceOk = false;
-      }
-      if (minStars && minStars > 0 && product.hasClass('abw-with-data')) {
-        priceOk = priceOk && nbStars >= minStars;
-      }
-      if (hideNsfw && product.hasClass('abw-nsfw')) {
-        priceOk = false;
-      }
-      if (priceOk) {
-        product.show('fast');
-      } else {
-        product.hide('fast');
-      }
-      // product.toggle(priceOk);
-      if (priceOk && !product.hasClass('abw-on-hover')) {
-        product.addClass('abw-on-hover');
-        product.hover(getData);
-      }
-    });
   }
 
   // insert controllers
@@ -178,16 +189,17 @@ $(document).ready(function() {
     $('#header-left').after(html);
   }
 
-  // restore previous choices
+  // get elements
   var hideFreeCheckbox = $('#wtc_hide_free');
-  hideFreeCheckbox.attr('checked', localStorage.abwHideFree === 'true'); // only if at true
   var hideNsfwCheckbox = $('#wtc_hide_nsfw');
-  hideNsfwCheckbox.attr('checked', localStorage.abwHideNsfw !== 'false'); // default true
-  var minStars = $('#wtc_min_stars');
-  minStars.val(parseInt(localStorage.abwMinStars) || 1);
+  var minStarsInput = $('#wtc_min_stars');
+  var minPriceInput = $('#wtc_min_price');
+  var maxPriceInput = $('#wtc_max_price');
 
-  // prepare a debounced function
-  var showHideProductsDebounced = debounce(showHideProducts, 1000);
+  // restore previous choices
+  hideFreeCheckbox.attr('checked', localStorage.abwHideFree === 'true'); // only if at true
+  hideNsfwCheckbox.attr('checked', localStorage.abwHideNsfw !== 'false'); // default true
+  minStarsInput.val(parseInt(localStorage.abwMinStars) || 1);
 
   // start filtering by default
   setTimeout(() => {
@@ -195,12 +207,33 @@ $(document).ready(function() {
     getNextData();
   }, 1000);
 
-  // when window is scrolled
-  window.onscroll = showHideProductsDebounced;
-
   // when input value change
-  $('#wtc_hide_free').change(showHideProductsDebounced);
-  $('#wtc_min_price').keydown(showHideProductsDebounced);
-  $('#wtc_max_price').keydown(showHideProductsDebounced);
-  $('#wtc_min_stars').keydown(showHideProductsDebounced);
+  hideFreeCheckbox.change((event) => {
+    hideFree = event.currentTarget.checked;
+    localStorage.abwHideFree = hideFree;
+    // console.log('hideFree is now', hideFree);
+    showHideProductsDebounced();
+  });
+  hideNsfwCheckbox.change((event) => {
+    hideNsfw = event.currentTarget.checked;
+    localStorage.abwHideNsfw = hideNsfw;
+    // console.log('hideNsfw is now', hideNsfw);
+    showHideProductsDebounced();
+  });
+  minPriceInput.change((event) => {
+    minPrice = parseInt(event.currentTarget.value) || 0;
+    // console.log('minPrice is now', minPrice);
+    showHideProductsDebounced();
+  });
+  maxPriceInput.change((event) => {
+    maxPrice = parseInt(event.currentTarget.value) || 1000;
+    // console.log('maxPrice is now', maxPrice);
+    showHideProductsDebounced();
+  });
+  minStarsInput.change((event) => {
+    minStars = parseInt(event.currentTarget.value);
+    localStorage.abwMinStars = minStars;
+    // console.log('minStars is now', minStars);
+    showHideProductsDebounced();
+  });
 });
