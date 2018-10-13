a7d4d1210c578b9292e703f79ccf49d202b425bc
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-10-13 11:16:47 +0200

    :rotating_light: optimized thanks to shellcheck

--- linux-utils/install.sh
+++ linux-utils/install.sh
@@ -15,7 +15,7 @@ reset
 #  ██╔══╝  ██║   ██║██║╚██╗██║██║        ██║   ██║██║   ██║██║╚██╗██║╚════██║
 #  ██║     ╚██████╔╝██║ ╚████║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║███████║
 #  ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
-                                                                                                                                                    
+
 function not_installed {
     # set to 1 (false) initially
     local return_=1
@@ -27,7 +27,7 @@ function not_installed {
 }
 
 function check_install {
-    if not_installed "$1" ; then    
+    if not_installed "$1" ; then
         consoleError "$1 has not been installed"
     else
         consoleSuccess "$1 has been installed"
@@ -35,26 +35,26 @@ function check_install {
 }
 
 function consoleError {
-    printf "\nERROR : ${1} \n" >> ${logfile} 2>&1
-    printf "\n  \e[31m✘ ${1}" # echo first argument in red   
-    echo -e "\033[0m" # reset colours back to normal
+    printf "\\nERROR : %s \\n" "${1}" >> ${logfile} 2>&1
+    printf "\\n  \\e[31m✘ %s" "${1}" # echo first argument in red
+    echo -e "\\033[0m" # reset colours back to normal
 }
 
-function consoleLog {    
-    printf "\nLOG : ${1} \n" >> ${logfile} 2>&1
-    printf "\n  \e[34m✔ ${1}" # echo first argument in blue
-    echo -e "\033[0m" # reset colours back to normal
+function consoleLog {
+    printf "\\nLOG : %s \\n" "${1}" >> ${logfile} 2>&1
+    printf "\\n  \\e[34m✔ %s" "${1}" # echo first argument in blue
+    echo -e "\\033[0m" # reset colours back to normal
 }
 
 function consoleSuccess {
-    printf "\nSUCCESS : ${1} \n" >> ${logfile} 2>&1
-    printf "\n  \e[32m✔ ${1}" # echo first argument in green
-    echo -e "\033[0m" # reset colours back to normal
+    printf "\\nSUCCESS : %s \\n" "${1}" >> ${logfile} 2>&1
+    printf "\\n  \\e[32m✔ %s" "${1}" # echo first argument in green
+    echo -e "\\033[0m" # reset colours back to normal
 }
 
 function install_if_needed {
     if not_installed "$1"; then
-        sudo apt-get install "$1" -y >> ${logfile} 2>&1
+        sudo apt-get install "$1" -y | sudo tee -a ${logfile} > /dev/null
         check_install "$1"
     else
         consoleLog "$1 was already installed"
@@ -64,7 +64,7 @@ function install_if_needed {
 function is_desktop {
     # set to 1 (false) initially
     local return_=1
-    # array of desktop packages 
+    # array of desktop packages
     packages=("ubuntu-desktop" "mate-desktop" "xubuntu-desktop" "elementary-desktop" "cinnamon-desktop-data" "gnome-desktop3-data")
     # check for one of them
     for package in "${packages[@]}"; do
@@ -77,7 +77,7 @@ function is_desktop {
             # stop iterations
             break
         fi
-    done 
+    done
     # return value
     return "$return_"
 }
@@ -85,8 +85,8 @@ function is_desktop {
 
 #   ██████╗██╗   ██╗███████╗████████╗ ██████╗ ███╗   ███╗██╗███████╗███████╗
 #  ██╔════╝██║   ██║██╔════╝╚══██╔══╝██╔═══██╗████╗ ████║██║╚══███╔╝██╔════╝
-#  ██║     ██║   ██║███████╗   ██║   ██║   ██║██╔████╔██║██║  ███╔╝ █████╗  
-#  ██║     ██║   ██║╚════██║   ██║   ██║   ██║██║╚██╔╝██║██║ ███╔╝  ██╔══╝  
+#  ██║     ██║   ██║███████╗   ██║   ██║   ██║██╔████╔██║██║  ███╔╝ █████╗
+#  ██║     ██║   ██║╚════██║   ██║   ██║   ██║██║╚██╔╝██║██║ ███╔╝  ██╔══╝
 #  ╚██████╗╚██████╔╝███████║   ██║   ╚██████╔╝██║ ╚═╝ ██║██║███████╗███████╗
 #   ╚═════╝ ╚═════╝ ╚══════╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═╝╚══════╝╚══════╝
 
@@ -98,18 +98,22 @@ fi
 
 # install custom mybashrc
 consoleLog "install or update custom ~/.mybashrc"
-sudo cp .mybashrc ~/.mybashrc --force --verbose >> ${logfile} 2>&1
+sudo cp .mybashrc ~/.mybashrc --force --verbose | sudo tee -a ${logfile} > /dev/null
+# shellcheck source=/dev/null
 source ~/.bashrc
 
 # prepare auto source at login
-if [[ -z $(grep ". ~/.mybashrc" ~/.bashrc) ]]; then    
+if ! grep -i 'mybashrc' ~/.bashrc # not(grep --ignore-case "mystring" in/this/file)
+then
     echo "source ~/.mybashrc" >> ~/.bashrc
     consoleSuccess "auto-source custom mybashrc at login"
+else
+    printf "\\n SUCCESS : mybashrc already auto-sourced in ~/.bashrc \\n" >> ${logfile} 2>&1
 fi
 
 # install custom utils, why not just add folder to PATH ?
 consoleLog "install custom utils"
-sudo cp -R mybins/* /usr/local/bin/ --force --verbose >> ${logfile} 2>&1
+sudo cp -R mybins/* /usr/local/bin/ --force --verbose | sudo tee -a ${logfile} > /dev/null
 sudo chmod +x /usr/local/bin/*
 
 # remove useless aptitude translations
@@ -122,11 +126,11 @@ sudo chmod +x /usr/local/bin/*
 # fi
 
 # clean shit
-sudo apt-get purge banshee brasero brasero-common brasero-cdrkit hexchat hexchat-common -y >> ${logfile} 2>&1 # audio + burner + chat
-sudo apt-get purge mate-screensaver mate-screensaver-common xscreensaver-data-extra xscreensaver-data xscreensaver-gl-extra xscreensaver-gl -y >> ${logfile} 2>&1 # screensavers
-sudo apt-get purge tomboy toshset brltty xplayer xplayer-common bluez-cups caja-folder-color-switcher -y >> ${logfile} 2>&1 # note + toshiba + braille display + player + bluetooth printers + custo
-sudo apt-get purge ideviceinstaller xserver-xorg-input-wacom xserver-xorg-video-vmware  >> ${logfile} 2>&1 # apple device handler + tablet + vmware
-sudo apt-get purge firefox thunderbird transmission pidgin gimp gimp-data -y >> ${logfile} 2>&1 # default mint/ubuntu programs
+sudo apt-get purge banshee brasero brasero-common brasero-cdrkit hexchat hexchat-common -y | sudo tee -a ${logfile} > /dev/null # audio + burner + chat
+sudo apt-get purge mate-screensaver mate-screensaver-common xscreensaver-data-extra xscreensaver-data xscreensaver-gl-extra xscreensaver-gl -y | sudo tee -a ${logfile} > /dev/null # screensavers
+sudo apt-get purge tomboy toshset brltty xplayer xplayer-common bluez-cups caja-folder-color-switcher -y | sudo tee -a ${logfile} > /dev/null # note + toshiba + braille display + player + bluetooth printers + custo
+sudo apt-get purge ideviceinstaller xserver-xorg-input-wacom xserver-xorg-video-vmware  | sudo tee -a ${logfile} > /dev/null # apple device handler + tablet + vmware
+sudo apt-get purge firefox thunderbird transmission pidgin gimp gimp-data -y | sudo tee -a ${logfile} > /dev/null # default mint/ubuntu programs
 consoleLog "cleaned unused packages"
 
 # system tweaks
@@ -137,10 +141,10 @@ if [[ ! -f ~/.lu-once ]]; then
     sudo iw reg set FR
     consoleSuccess "wifi region settings set to EU"
 
-    sudo sh -c "echo 'Dir::Cache \"\";\nDir::Cache::archives \"\";' >> /etc/apt/apt.conf.d/02nocache"
+    sudo sh -c "echo 'Dir::Cache \"\";\\nDir::Cache::archives \"\";' >> /etc/apt/apt.conf.d/02nocache"
     consoleSuccess "disabled apt local cache"
 
-    sudo sh -c "echo '\nHISTFILESIZE=10000\nHISTSIZE=10000\nHISTCONTROL=ignoredups' >> /etc/environment"
+    sudo sh -c "echo '\\nHISTFILESIZE=10000\\nHISTSIZE=10000\\nHISTCONTROL=ignoredups' >> /etc/environment"
     consoleSuccess "allowed 10k entries in history instead of 500 by default"
 
     echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
@@ -153,20 +157,20 @@ fi
 
 #  ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗
 #  ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
-#  ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  
-#  ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  
+#  ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗
+#  ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝
 #  ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗
 #   ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝
 
 consoleLog "updating apt repositories..."
-sudo apt-get update >> ${logfile} 2>&1
+sudo apt-get update | sudo tee -a ${logfile} > /dev/null
 consoleLog "update apt complete !"
 
 
-#  ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     
-#  ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     
-#  ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     
-#  ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     
+#  ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗
+#  ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║
+#  ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║
+#  ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║
 #  ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗
 #  ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝
 
@@ -175,6 +179,9 @@ install_if_needed "software-properties-common"  # packages needed to have "add-a
 install_if_needed "lsb-release"     # lsb-release to show current system
 install_if_needed "curl"            # to dl stuff
 install_if_needed "build-essential" # to build stuff
+install_if_needed "gcc"             # to build stuff (and needed for VBoxGuestAdditions)
+install_if_needed "make"            # to build stuff (and needed for VBoxGuestAdditions)
+install_if_needed "perl"            # to build stuff (and needed for VBoxGuestAdditions)
 install_if_needed "nano"            # mini editor
 install_if_needed "htop"            # better than top
 install_if_needed "ncdu"            # browsable tree size
@@ -184,10 +191,10 @@ install_if_needed "hdparm"          # good at measuring disk speed
 install_if_needed "screenfetch"     # screenfetch is a kikoo login ascii art
 install_if_needed "pm-utils"        # allow easy access to suspend, hibernate etc..
 install_if_needed "git"             # really need a comment ?
-install_if_needed "libssl-dev"      # pretty commom dev dep 
-install_if_needed "libcurl4-openssl-dev" # pretty commom dev dep 
+install_if_needed "libssl-dev"      # pretty commom dev dep
+install_if_needed "libcurl4-openssl-dev" # pretty commom dev dep
 
-# great tool to record and share terminal sessions 
+# great tool to record and share terminal sessions
 # install_if_needed "asciinema"
 # to start just run "asciinema rec", to finish hit Ctrl-D or type "exit"
 
@@ -200,7 +207,7 @@ install_if_needed "libimage-exiftool-perl"
 # de/compression libs
 app="p7zip-full"
 if not_installed ${app} ; then
-    sudo apt-get install p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller arj -y >> ${logfile} 2>&1
+    sudo apt-get install p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller arj -y | sudo tee -a ${logfile} > /dev/null
     consoleSuccess "installed commons de/compression libs"
 else
     consoleLog "de/compression libs was already installed"
@@ -213,12 +220,12 @@ else
     consoleLog "Node Version Manager was already installed"
 fi
 
-#  ██████╗ ███████╗███████╗██╗  ██╗████████╗ ██████╗ ██████╗ 
+#  ██████╗ ███████╗███████╗██╗  ██╗████████╗ ██████╗ ██████╗
 #  ██╔══██╗██╔════╝██╔════╝██║ ██╔╝╚══██╔══╝██╔═══██╗██╔══██╗
 #  ██║  ██║█████╗  ███████╗█████╔╝    ██║   ██║   ██║██████╔╝
-#  ██║  ██║██╔══╝  ╚════██║██╔═██╗    ██║   ██║   ██║██╔═══╝ 
-#  ██████╔╝███████╗███████║██║  ██╗   ██║   ╚██████╔╝██║     
-#  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝     
+#  ██║  ██║██╔══╝  ╚════██║██╔═██╗    ██║   ██║   ██║██╔═══╝
+#  ██████╔╝███████╗███████║██║  ██╗   ██║   ╚██████╔╝██║
+#  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝
 
 if ! is_desktop ; then
     bash
@@ -247,7 +254,7 @@ install_if_needed "chromium-browser" # as good as chrome but without spywares # 
 # Unix FireWall
 app="gufw"
 if not_installed ${app} ; then
-    sudo ufw enable >> ${logfile} 2>&1 # enable
+    sudo ufw enable | sudo tee -a ${logfile} > /dev/null # enable
     install_if_needed "gufw"         # gui for UFW
 else
     consoleLog "${app} was already installed"
@@ -256,9 +263,9 @@ fi
 # microsoft fonts + unicode
 app="ttf-dejavu"
 if not_installed ${app} ; then
-    sudo apt-get install ttf-ubuntu-font-family ttf-dejavu ttf-dejavu-extra ttf-liberation ttf-ancient-fonts -y >> ${logfile} 2>&1
+    sudo apt-get install ttf-ubuntu-font-family ttf-dejavu ttf-dejavu-extra ttf-liberation ttf-ancient-fonts -y | sudo tee -a ${logfile} > /dev/null
     consoleSuccess "installed extra fonts"
-    sudo fc-cache -f -v >> ${logfile} 2>&1
+    sudo fc-cache -f -v | sudo tee -a ${logfile} > /dev/null
     consoleSuccess "cleared & reloaded font cache"
 else
     consoleLog "extra fonts was already installed"
@@ -269,7 +276,7 @@ install_if_needed "redshift"
 install_if_needed "gtk-redshift"
 
 # to read dvd (commented because not using that much)
-#app="libdvdcss2" 
+#app="libdvdcss2"
 #if not_installed ${app} ; then
 #    sudo gdebi --non-interactive --quiet saveddeb/libdvdcss2_*.deb >> ${logfile} 2>&1 # simple library designed for accessing DVDs
 #    check_install ${app}
@@ -279,11 +286,11 @@ install_if_needed "gtk-redshift"
 #install_if_needed "libdvdnav4"
 
 # great app/file launcher like launchy
-sudo gdebi --non-interactive --quiet saveddeb/ulauncher_*.deb >> ${logfile} 2>&1 
+sudo gdebi --non-interactive --quiet saveddeb/ulauncher_*.deb | sudo tee -a ${logfile} > /dev/null
 consoleLog "uLauncher installed or updated"
 
 # clean apt packages
-sudo apt-get autoremove -y >> ${logfile} 2>&1
+sudo apt-get autoremove -y | sudo tee -a ${logfile} > /dev/null
 
 app="ttf-mscorefonts-installer"
 if not_installed ${app} ; then
@@ -304,14 +311,14 @@ exit 1;
 #     ██║   ██║  ██║██║  ██║██║ ╚████║██║  ██╗███████║
 #     ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
 
-# For ascii art                                                     
+# For ascii art
 # http://patorjk.com/software/taag/#p=display&h=0&v=0&c=bash&f=ANSI%20Shadow&t=THANKS
 
 # For functions
-# https://gist.github.com/JamieMason/4761049   
+# https://gist.github.com/JamieMason/4761049
 # http://askubuntu.com/questions/12562/how-to-check-if-ubuntu-desktop-or-server-is-installed#12571
 
-# For custom bashrc 
+# For custom bashrc
 # https://github.com/gto76/standard-aliases
 
 # For ideas & tweaks
@@ -319,10 +326,10 @@ exit 1;
 # http://www.noobslab.com/2013/10/tweaksthings-to-do-after-install-of.html
 
 
-#  ████████╗██╗  ██╗███████╗    ███████╗███╗   ██╗██████╗ 
+#  ████████╗██╗  ██╗███████╗    ███████╗███╗   ██╗██████╗
 #  ╚══██╔══╝██║  ██║██╔════╝    ██╔════╝████╗  ██║██╔══██╗
 #     ██║   ███████║█████╗      █████╗  ██╔██╗ ██║██║  ██║
 #     ██║   ██╔══██║██╔══╝      ██╔══╝  ██║╚██╗██║██║  ██║
 #     ██║   ██║  ██║███████╗    ███████╗██║ ╚████║██████╔╝
-#     ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝╚═╝  ╚═══╝╚═════╝ 
-# 
+#     ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝╚═╝  ╚═══╝╚═════╝
+#
