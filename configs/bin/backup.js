bc0ef813510ab4703f1d23a07b502ccd806af748
Romain Racamier <romain.racamier@externe.e-i.com>
2019-03-20 15:35:26 +0100

    added backup util

---
+++ configs/bin/backup.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+const home = process.env.HOME
+const appData = process.env.APPDATA || (process.platform === 'darwin' ? home + 'Library/Preferences' : home + '/.local/share')
+const configs = [`${home}/.hyper.js`, { file: `${appData}/Code/User/settings.json`, renameTo: `vscode.json` }, `${home}/.gitignore`]
+const backupPath = process.env.PWD
+const debug = true
+const processOne = false
+
+function copy (source, dest) {
+  if (debug) {
+    console.log('copy from :', source)
+    console.log('copy to   :', dest)
+  }
+  // destination.txt will be created or overwritten by default.
+  fs.copyFile(source, dest, (err) => {
+    if (err) throw err
+    console.log('backed up :', source)
+  })
+}
+
+function backup (config) {
+  let source = ''
+  let filename = ''
+  if (typeof config === 'string') {
+    source = config
+    filename = path.basename(config)
+  } else if (typeof config === 'object') {
+    source = config.file
+    filename = config.renameTo
+  } else {
+    throw new Error('unhandled config format')
+  }
+  const dest = path.join(backupPath, filename)
+  copy(source, dest)
+}
+
+function start () {
+  console.log('starting configs backup')
+  if (processOne) {
+    backup(configs[0])
+  } else {
+    configs.map(config => backup(config))
+  }
+}
+
+start()
