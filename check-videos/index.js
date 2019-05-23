1b4ad117842ea3b3247a4b7545610f57bb75ef36
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-05-23 23:59:17 +0200

    now with progress bar, recursive, complete ext

--- check-videos/index.js
+++ check-videos/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const find = require('globby')
 const fs = require('fs')
 const duration = require('get-video-duration').default
+const ProgressBar = require('progress')
 
 const debug = false
 
@@ -21,13 +22,12 @@ function getFileSizeInMB (filename) {
 
 async function checkVideo (videoPath) {
   return duration(videoPath).then(seconds => {
-    const minutes = Math.round(seconds / 60)
+    const minutes = Math.round(seconds / 60) || 1
     const size = getFileSizeInMB(videoPath)
     const ratio = Math.round(size / minutes)
-    const file = `(${ratio}) ${path.basename(videoPath)}`
+    const file = `(${ratio}) ${size} mb - ${videoPath}`
     if (debug) {
-      // log('checked file', `(${ratio}) ${path.basename(videoPath)} is ${minutes} min long and weight ${size} mb, around ${ratio} mb per minute`)
-      log('checked file', file)
+      log('checked file', `${file} is ${minutes} min long`)
     }
     if (ratio > 30) {
       return Promise.resolve(file)
@@ -35,21 +35,44 @@ async function checkVideo (videoPath) {
   })
 }
 
-async function checkVideos () {
+async function findVideos () {
   if (process.argv.length <= 2) {
     log('use me via', 'node ' + path.basename(__filename) + ' path/to/directory')
     return process.exit(-1)
   }
   const videosPath = process.argv[2].replace('\\', '//')
   log('scanning dir', videosPath)
-  const pattern = videosPath + '/*.{mp4,mkv}'
+  const pattern = videosPath + '/**/*.{mp4,mkv,avi,wmv,m4v,mpg}'
   log('with pattern', pattern)
-  return find(pattern).then(files => Promise.all(files.map(checkVideo)))
+  return find(pattern)
 }
 
-checkVideos()
-  .then((list) => {
-    list = list.filter(item => item !== undefined)
-    log('found', `${list.length} videos with high ratio`)
-    list.map(file => console.log('-', file))
+async function checkVideos (files) {
+  const bar = new ProgressBar('processing   : [:bar] file :current/:total', {
+    complete: '=',
+    incomplete: ' ',
+    total: files.length,
+    width: 40
   })
+  return Promise.all(files.map(file => {
+    bar.tick()
+    return checkVideo(file)
+  }))
+}
+
+function showReport(list) {
+  let report = ''
+  list = list.filter(file => {
+    if(file === undefined) {
+      return false
+    }
+    report += `- ${file}\n`
+    return true
+  })
+  log('found', `${list.length} videos with high ratio`)
+  console.log(report)
+}
+
+findVideos()
+  .then(checkVideos)
+  .then(showReport)
