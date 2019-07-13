1dd6f35dabdbf5872ba6e875dbc2e8b111707f1a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-07-13 14:51:30 +0200

    enhance log

--- configs/bin/backup.js
+++ configs/bin/backup.js
@@ -7,18 +7,22 @@ const home = process.env.HOME
 const appData = process.env.APPDATA || (process.platform === 'darwin' ? home + 'Library/Preferences' : home + '/.local/share')
 const configs = [`${appData}/Hyper/.hyper.js`, { file: `${appData}/Code/User/settings.json`, renameTo: 'vscode.json' }, `${home}/.gitignore`, `${home}/.gitconfig`]
 const backupPath = path.join(process.env.PWD, '/files')
-const debug = true
-const processOne = false
 
 function copy (source, dest) {
-  if (debug) {
-    console.log('copy from :', source)
-    console.log('copy to   :', dest)
-  }
-  // destination.txt will be created or overwritten by default.
-  fs.copyFile(source, dest, (err) => {
-    if (err) throw err
-    console.log('backed up :', source)
+  return new Promise(resolve => {
+    source = path.normalize(source)
+    dest = path.normalize(dest)
+    // destination.txt will be created or overwritten by default.
+    fs.copyFile(source, dest, (err) => {
+      console.log('')
+      if (err) {
+        console.log('no file   :', source)
+      } else {
+        console.log('backed up :', source)
+        console.log('to        :', dest)
+      }
+      resolve()
+    })
   })
 }
 
@@ -35,17 +39,13 @@ function backup (config) {
     throw new Error('unhandled config format')
   }
   const dest = path.join(backupPath, filename)
-  copy(source, dest)
+  return copy(source, dest)
 }
 
-function start () {
-  console.log('starting configs backup')
-  if (processOne) {
-    backup(configs[0])
-  } else {
-    configs.map(config => backup(config))
-  }
-  console.log('\n Tip: you can use "git difftool my-file.json" to check updates\n\n Configs backup done.\n')
+async function start () {
+  console.log('\nConfigs backup start')
+  await Promise.all(configs.map(config => backup(config)))
+  console.log('\nTip: you can use "git difftool files\\my-file.json" to check updates\n\nConfigs backup done.\n')
 }
 
 start()
