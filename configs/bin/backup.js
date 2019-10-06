d6cba5fa74bbfe3acc56b999c893b4dc5941c82a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-10-06 21:00:55 +0200

    handle auto merge

--- configs/bin/backup.js
+++ configs/bin/backup.js
@@ -3,28 +3,47 @@ const files = require('./files')
 const utils = require('./utils')
 
 async function backup (file) {
-  const filesEqual = await utils.areFilesEqual(file.source, file.dest)
-  if (filesEqual) {
-    return utils.log('backup is up to date for :', file.source)
+  const source = await utils.report(file.source)
+  const dest = await utils.report(file.dest)
+  if (!source.exists) {
+    return utils.log('source file does not exists :', source.path)
   }
-  const copySuccess = await utils.copy(file.source, file.dest)
-  if (!copySuccess) {
-    return utils.log('no file or error at copying :', file.source)
+  if (!dest.exists) {
+    const success = await utils.copy(source.path, dest.path)
+    if (success) {
+      return utils.log('backed up :', source.path)
+    }
+    return utils.log('failed at copying :', source.path)
   }
-  utils.log('backed up :', file.source)
+  const sameContent = await utils.equals(source.content, dest.content)
+  if (sameContent) {
+    return utils.log('backup is up to date :', source.path)
+  }
+  const isJson = source.path.includes('.json')
+  if (isJson) {
+    utils.log('cannot auto back up json file :', source.path)
+  } else {
+    // merge fail beautifully with json files \o/
+    const filesMerged = await utils.merge(source.path, dest.path)
+    if (!filesMerged) {
+      return utils.log('failed to merge file :', source.path)
+    }
+    utils.log('backed up :', source.path)
+  }
   const destFile = 'files/' + file.dest.split('configs\\files\\')[1]
-  return 'git difftool ' + destFile
-  // utils.log('2. backup  < > local  : merge', destFile, file.source.replace(/\\/g, '/'))
+  return `merge ${destFile} ${utils.normalize(source.path, true)}`
 }
 
 async function start () {
-  utils.log('Backup start...')
+  utils.log('Backup start...\n')
   const results = await Promise.all(files.map(file => backup(file)))
   const suggestedCommands = results.filter(result => !!result)
   if (suggestedCommands.length) {
-    utils.log('suggested commands :\n\n', suggestedCommands.join('\n '))
+    utils.log('\nTODO :\n=====\n1. review changes on this repo if any\n2. run these to compare backup & local files :\n\n', suggestedCommands.join('\n '))
+  }else {
+    utils.log('\nNo actions required.')
   }
-  utils.log('Backup done.')
+  utils.log('\nBackup done, have a nice day :)')
 }
 
 start()
--- configs/bin/utils.js
+++ configs/bin/utils.js
@@ -1,39 +1,64 @@
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
-
+const exec = util.promisify(require('child_process').exec);
 const readFile = util.promisify(fs.readFile)
+const copyFile = util.promisify(fs.copyFile)
+const writeFile = util.promisify(fs.writeFile)
+const deleteFile = util.promisify(fs.unlink)
+const statFile = util.promisify(fs.stat)
 
-const log = console.log.bind(console, '\n')
+const log = console.log.bind(console, '')
 
 function clean (str) {
   return str.replace(/[\r\n\s]*/g, '')
 }
 
-function areFilesEqual (file1, file2) {
-  return Promise.all([readFile(file1, 'utf-8'), readFile(file2, 'utf-8')])
-    .then(([content1, content2]) => {
-      const areEqual = (clean(content1) === clean(content2))
-      // log('file 1 :', file1)
-      // log('file 2 :', file2)
-      // log('are equal ?', areEqual)
-      return areEqual
-    })
-    .catch(err => {
-      if (!err.message.includes('no such file')) {
-        console.error(err)
-      }
-      return false
-    })
+function equals (content1, content2) {
+  return (clean(content1) === clean(content2))
 }
 
-function copy (source, dest) {
-  return new Promise(resolve => {
-    const from = path.normalize(source)
-    const to = path.normalize(dest)
-    // destination will be created or overwritten by default.
-    fs.copyFile(from, to, (err) => resolve(!err))
+function normalize (filepath, useSlash) {
+  const p = path.normalize(filepath)
+  return useSlash ? p.replace(/\\/g, '/') : p
+}
+
+async function read (path) {
+  return readFile(path, 'utf-8').catch((err) => {
+    if (!err.message.includes('no such file')) {
+      console.error(err)
+    }
+    return false
   })
 }
 
-module.exports = { areFilesEqual, copy, log }
+async function report (filepath) {
+  const content = await read(filepath)
+  return {
+    path: normalize(filepath),
+    exists: !!content,
+    content
+  }
+}
+
+async function copy (source, dest) {
+  // destination will be created or overwritten by default.
+  return copyFile(source, dest).then(() => true).catch((err) => {
+    log(err)
+    return false
+  })
+}
+
+async function merge (source, dest) {
+  const empty = 'omg.txt'
+  await writeFile(empty, '').catch((err) => {
+    log(err)
+    return false
+  })
+  // git merge-file b.txt c.txt a.txt -p
+  await exec(`git merge-file -L "last backup" -L useless -L "local file" ${normalize(dest, true)} ${empty} ${normalize(source, true)}`).catch(() => true)
+  await deleteFile(empty)
+  return true
+}
+
+module.exports = { equals, copy, log, merge, report, read, normalize }
