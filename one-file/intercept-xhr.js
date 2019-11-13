5555673a9a56b788cd2e4763cedee2d33bcb40cd
Romain Racamier <romain.racamier@externe.e-i.com>
2019-11-13 17:35:21 +0100

    add xhr interceptor

---
+++ one-file/intercept-xhr.js
@@ -0,0 +1,21 @@
+const lastRequest = { url: '', method: '' }
+
+const OldXHR = window.XMLHttpRequest
+function newXHR () {
+  const instance = new OldXHR()
+  instance.addEventListener('readystatechange', () => {
+    if (instance.readyState === 4 && instance.status !== 200) {
+      console.log(`HTTP Error ${instance.status} on ${lastRequest.method} ${lastRequest.url}`)
+    }
+  }, false)
+  return instance
+}
+
+const originalOpen = OldXHR.prototype.open
+OldXHR.prototype.open = function open (method, url) {
+  lastRequest.method = method
+  lastRequest.url = url
+  return originalOpen.apply(this, [].slice.call(arguments))
+}
+
+window.XMLHttpRequest = newXHR
