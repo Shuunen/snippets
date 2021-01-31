4e900fd0be68548acede354f7aae4cae3b756aca
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-01-31 18:41:55 +0100

    handle setup & dry run

--- configs/bin/backup.js
+++ configs/bin/backup.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 const files = require('./files')
 const utils = require('./utils')
+const dry = process.argv.includes('--dry')
+const setup = process.argv.includes('--setup')
 
 async function backup (file) {
   const source = await utils.report(file.source)
   const destination = await utils.report(file.dest)
-  if (!source.exists) return utils.log('source file does not exists :', source.path)
+  if (!source.exists) {
+    if(!setup) return utils.log('source file does not exists :', source.path)
+    if (dry) return utils.log(`would copy ${utils.filename(destination.path)} to ${source.path}`)
+    const success = await utils.copy(destination.path, source.path)
+    if (success) return utils.log('file setup :', source.path)
+    return utils.log('failed at copying :', destination.path)
+  }  
   if (!destination.exists) {
+    if (dry) return utils.log(`would copy ${source.path} to ${destination.path}`)
     const success = await utils.copy(source.path, destination.path)
     if (success) return utils.log('backed up :', source.path)
     return utils.log('failed at copying :', source.path)
@@ -19,26 +28,25 @@ async function backup (file) {
     utils.log('cannot auto back up file :', source.path)
   } else {
     // merge fail beautifully with json files \o/
+    if (dry) return utils.log(`would merge "${source.path}" to "${destination.path}"`)
     const filesMerged = await utils.merge(source.path, destination.path)
     if (!filesMerged) {
       return utils.log('failed to merge file :', source.path)
     }
     utils.log('backed up :', source.path)
   }
-  const destinationFile = 'files/' + file.dest.split('configs\\files\\')[1]
-  return `merge ${destinationFile} ${utils.normalize(source.path, true)}`
+  return `merge files/${utils.filename(file.dest)} ${utils.normalize(source.path, true)}`
 }
 
 async function start () {
-  utils.log('Backup start...\n')
+  utils.log('\n Backup start...\n')
   const results = await Promise.all(files.map(file => backup(file)))
   const suggestedCommands = results.filter(result => !!result)
   if (suggestedCommands.length > 0) {
-    utils.log('\nTODO :\n=====\n1. review changes on this repo if any\n2. run these to compare backup & local files :\n\n', suggestedCommands.join('\n '))
+    utils.log('\n TODO :\n=====\n1. review changes on this repo if any\n2. run these to compare backup & local files :\n\n', suggestedCommands.join('\n '),'\n')
   } else {
-    utils.log('\nNo actions required.')
+    utils.log('\n No actions required.\n')
   }
-  utils.log('\nBackup done, have a nice day :)')
 }
 
 start()
