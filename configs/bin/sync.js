e586c43a95e8d4cb162ecf710e85838aacbce109
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-01-31 18:47:13 +0100

    rename to sync because it is what it is

--- configs/bin/sync.js
+++ configs/bin/sync.js
@@ -4,7 +4,7 @@ const utils = require('./utils')
 const dry = process.argv.includes('--dry')
 const setup = process.argv.includes('--setup')
 
-async function backup (file) {
+async function sync (file) {
   const source = await utils.report(file.source)
   const destination = await utils.report(file.dest)
   if (!source.exists) {
@@ -17,30 +17,30 @@ async function backup (file) {
   if (!destination.exists) {
     if (dry) return utils.log(`would copy ${source.path} to ${destination.path}`)
     const success = await utils.copy(source.path, destination.path)
-    if (success) return utils.log('backed up :', source.path)
+    if (success) return utils.log('sync done :', source.path)
     return utils.log('failed at copying :', source.path)
   }
   const sameContent = await utils.equals(source.content, destination.content)
-  if (sameContent) return utils.log('backup is up to date :', source.path)
+  if (sameContent) return utils.log('sync is up to date :', source.path)
   const isJson = source.path.includes('.json')
   const isIni = source.path.includes('.ini')
   if (isJson || isIni) {
-    utils.log('cannot auto back up file :', source.path)
+    utils.log('cannot auto sync file :', source.path)
   } else {
     // merge fail beautifully with json files \o/
-    if (dry) return utils.log(`would merge "${source.path}" to "${destination.path}"`)
+    if (dry) return utils.log(`would merge ${source.path} to ${destination.path}`)
     const filesMerged = await utils.merge(source.path, destination.path)
     if (!filesMerged) {
       return utils.log('failed to merge file :', source.path)
     }
-    utils.log('backed up :', source.path)
+    utils.log('sync done :', source.path)
   }
   return `merge files/${utils.filename(file.dest)} ${utils.normalize(source.path, true)}`
 }
 
 async function start () {
-  utils.log('\n Backup start...\n')
-  const results = await Promise.all(files.map(file => backup(file)))
+  utils.log('\n Sync start...\n')
+  const results = await Promise.all(files.map(file => sync(file)))
   const suggestedCommands = results.filter(result => !!result)
   if (suggestedCommands.length > 0) {
     utils.log('\n TODO :\n=====\n1. review changes on this repo if any\n2. run these to compare backup & local files :\n\n', suggestedCommands.join('\n '),'\n')
--- configs/readme.md
+++ configs/readme.md
@@ -2,6 +2,10 @@
 
 This folder is a backup of software configurations I use
 
-## Update
+## Sync
 
-Just run `./bin/backup.js`
+```bash
+./bin/sync.js
+./bin/sync.js --setup # tell sync to create files if they does not exists
+./bin/sync.js --dry # show what sync want to do without modifying anything on the fs
+```
