64e3492fa8d86e04de39c4ba6f303b6f4ed62c2e
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-05-22 18:18:36 +0200

    fix lint

--- one-file/shrink-pdf.sh
+++ one-file/shrink-pdf.sh
@@ -7,48 +7,46 @@
 
 output="temp.pdf"
 
-shrink ()
-{
-	~/Apps/GhostScript/bin/gswin64c \
-	  -q -dNOPAUSE -dBATCH \
-	  -sDEVICE=pdfwrite \
-	  -dCompatibilityLevel=1.4 \
-	  -dPDFSETTINGS="//$2" \
-	  -sOutputFile="$output" \
-	  "$1"
+shrink() {
+  gswin64c \
+    -q -dNOPAUSE -dBATCH \
+    -sDEVICE=pdfwrite \
+    -dCompatibilityLevel=1.4 \
+    -dPDFSETTINGS="//$2" \
+    -sOutputFile="$output" \
+    "$1"
 }
 
-check_smaller ()
-{
-	if [ ! -f "$1" -o ! -f "$2" ]; then
-		return 0;
-	fi
-	input=$1
-	inSize="$(echo $(wc -c "$input") | cut -f1 -d\ )"
-	outSize="$(echo $(wc -c "$2") | cut -f1 -d\ )"
-	percent=$(( outSize*100/inSize*100/100 ))
-	if (( percent > 0 && percent < 99 )); then
-		mv "$output" "$input"
-		echo " $input : compressed, file size reduced to $percent% of the original."
-	else
-		rm "$output"
-		echo " $input : compressed file is not smaller, nothing done."
-	fi
+check_smaller() {
+  if [ ! -f "$1" -o ! -f "$2" ]; then
+    return 0
+  fi
+  input=$1
+  inSize="$(echo $(wc -c "$input") | cut -f1 -d\ )"
+  outSize="$(echo $(wc -c "$2") | cut -f1 -d\ )"
+  percent=$((outSize * 100 / inSize * 100 / 100))
+  if ((percent > 0 && percent < 99)); then
+    mv "$output" "$input"
+    echo " $input : compressed, file size reduced to $percent% of the original."
+  else
+    rm "$output"
+    echo " $input : compressed file is not smaller, nothing done."
+  fi
 }
 
 if [ ! -z "$1" ]; then
-	input="$1"
+  input="$1"
 else
-	echo ""
-	echo " Usage: shrink-pdf.sh my-file.pdf"
-	echo "  for a better quality : shrink-pdf.sh my-file.pdf 1"
-	exit 1
+  echo ""
+  echo " Usage: shrink-pdf.sh my-file.pdf"
+  echo "  for a better quality : shrink-pdf.sh my-file.pdf 1"
+  exit 1
 fi
 
 if [ ! -z "$2" ]; then
-	quality="default"
+  quality="default"
 else
-	quality="ebook"
+  quality="ebook"
 fi
 
 shrink "$input" "$quality" || exit $?
