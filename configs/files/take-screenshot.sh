9c037d74186eeb8657adb1cf02275332bb20e73b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-09-05 21:19:07 +0200

    enhance take screen with zenity

--- configs/files/take-screenshot.sh
+++ configs/files/take-screenshot.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ! command -v ffprobe &>/dev/null; then
+  zenity --error --text "Ffprobe is not installed" --width 300
+  exit 1
+fi
+
 time=$(zenity --entry --title="Take screenshot" --text="Please type the time in mmss or ss :" --entry-text="")
 
 node "$HOME/Projects/github/snippets/one-file/take-screenshot.js" "$1" "$time"
+
+zenity --notification --text "Screenshot taken to your image folder"
