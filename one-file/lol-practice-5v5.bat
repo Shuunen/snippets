3cb65db0b0379043ba8f88459dd6154ba0592277
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-01-10 10:43:57 +0100

    change lol location

--- one-file/lol-practice-5v5.bat
+++ one-file/lol-practice-5v5.bat
@@ -1,3 +1,3 @@
 @echo off
 
-node "%USERPROFILE%\Projects\github\snippets\one-file\lol-practice-5v5.js" "%USERPROFILE%\Games\Riot Games\League of Legends\lockfile"
+node "%USERPROFILE%\Projects\github\snippets\one-file\lol-practice-5v5.js" "C:\Riot Games\League of Legends\lockfile"
