adf83cc2571c5e6a681bae659eb4de23a8a434ce
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-02-16 16:07:45 +0100

    better placement & replace

--- one-file/repo-banner.cli.ts
+++ one-file/repo-banner.cli.ts
@@ -2,8 +2,9 @@ import { readFileSync, writeFileSync } from 'fs'
 import path from 'path'
 import { parseJson } from 'shuutils'
 import { fileURLToPath } from 'url'
-import { descFiller, logger, nameFiller, replaceAndCheck } from './repo-banner.utils.js'
+import { logger, replaceAndCheck, replaceAndCheckById } from './repo-banner.utils.js'
 
+// usage : ts-node-esm one-file/repo-banner.cli.ts
 
 const thisFilePath = fileURLToPath(import.meta.url)
 const thisFileFolder = path.dirname(thisFilePath)
@@ -19,33 +20,35 @@ interface PackageJson {
   name: string
   description: string
 }
-const defaults = { color: '#024eb8', name: 'unknown', description: 'A placeholder description' }
+const defaults = { color: '#024eb8', scope: 'JohnDoe', name: 'unknown', description: 'A placeholder description' }
 const rawJson = readFileSync(path.join(currentWorkingFolder, 'package.json'), 'utf8')
 const { error, value: packageJson } = parseJson<PackageJson>(rawJson)
 if (error) { logger.error(error); process.exit(1) }
 // eslint-disable-next-line etc/no-assign-mutated-array
-const name = /\.com\/[^/]+\/(?<name>[\w-]+)/iu.exec(rawJson)?.groups?.name ?? packageJson.name.split('/').reverse()[0] ?? defaults.name
+const scopeAndName = /\.com\/(?<scope>[^/]+)\/(?<name>[\w-]+)/iu.exec(rawJson)?.groups
+const name = scopeAndName?.name ?? packageJson.name.split('/').reverse()[0] ?? defaults.name
+const scope = scopeAndName?.scope ?? defaults.scope
 if (name === defaults.name) logger.error('Could not find a name for the project, using the default one :', name)
 const { description = defaults.description } = packageJson
 if (description === defaults.description) logger.error('Could not find a description for the project, using the default one :', description)
 // eslint-disable-next-line security/detect-unsafe-regex, unicorn/no-unsafe-regex
 const color = /#(?:[\da-f]{3}){1,2}/iu.exec(rawJson)?.[0] ?? defaults.color
 if (color === defaults.color) logger.warn('Could not find a color for the project, using the default one :', color)
-const data = { color, name, description }
+const data = { color, scope, name, description }
 
 /**
  * SVG generation
  */
 logger.info('Generating the SVG code with data :')
+logger.info('- scope :', data.scope)
 logger.info('- name :', data.name)
 logger.info('- description :', data.description)
 logger.info('- color :', data.color)
 const svgTemplate = readFileSync(path.join(thisFileFolder, 'repo-banner.template.svg'), 'utf8')
-let svg = replaceAndCheck(svgTemplate, /#f{6}/gu, data.color)
-svg = replaceAndCheck(svg, />name</gu, `>${data.name}<`)
-svg = replaceAndCheck(svg, />name-f</gu, `>${nameFiller(data.name)}<`)
-svg = replaceAndCheck(svg, />The description is kind of pretty !</gu, `>${data.description}<`)
-svg = replaceAndCheck(svg, />desc-filler</gu, `>${descFiller(data.description)}<`)
+let svg = replaceAndCheck(svgTemplate, /(?<before>: )(?<content>#f{6})(?<after>[\d]*")/gu, data.color)
+svg = replaceAndCheckById(svg, 'projectName', data.name)
+svg = replaceAndCheckById(svg, 'projectScope', data.scope)
+svg = replaceAndCheckById(svg, 'projectDescription', data.description)
 logger.info('SVG code prepared, writing to file...')
 const outFile = `${name}-banner.svg`
 writeFileSync(outFile, svg)
--- one-file/repo-banner.utils.ts
+++ one-file/repo-banner.utils.ts
@@ -1,54 +1,5 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
-
 import { Logger } from 'shuutils'
-
-/**
- * Calculate the filler length for the description
- * @param descLength The description length
- * @returns The filler length
- * @example descFillerLength(9) // 32
- * @example descFillerLength(14) // 27
- * @example descFillerLength(20) // 22
- * @example descFillerLength(23) // 21
- * @example descFillerLength(25) // 19
- * @example descFillerLength(32) // 13
- * @example descFillerLength(37) // 10
- * @example descFillerLength(41) // 6
- * @example descFillerLength(46) // 1
- */
-function descFillerLength (descLength: number): number {
-  if (descLength < 10) return 32
-  if (descLength < 15) return 27
-  if (descLength < 21) return 22
-  if (descLength < 24) return 21
-  if (descLength < 26) return 19
-  if (descLength < 33) return 13
-  if (descLength < 38) return 10
-  if (descLength < 42) return 6
-  if (descLength < 47) return 1
-  return 0
-}
-
-/**
- * Get the filler for the description
- * @param description The description to fill
- * @returns The filler
- * @example descFiller('The description is kind of pretty !') // '..............................................'
- * @example descFiller('The description') // '.................................'
- */
-export function descFiller (description: string): string {
-  return '.'.repeat(descFillerLength(description.length))
-}
-
-/**
- * Get the filler for the name
- * @param name The name to fill
- * @returns The filler
- * @example nameFiller('name') // '............'
- */
-export function nameFiller (name: string): string {
-  return '.'.repeat(Math.max(0, 16 - name.length))
-}
 
 export const logger = new Logger()
 
@@ -61,8 +12,20 @@ export const logger = new Logger()
  * @example replaceAndCheck('Hello world !', /world/gu, 'there') // 'Hello there !'
  */
 export function replaceAndCheck (string: string, regex: RegExp, replacement: string): string {
-  const replaced = string.replace(regex, replacement)
+  const replaced = string.replace(regex, `$<before>${replacement}$<after>`)
   if (replaced === string) logger.error(`Could not replace ${String(regex)} with ${replacement}`)
   return replaced
 }
 
+/**
+ * Replace a placeholder in a string and check if it was replaced via an identifier
+ * @param string The string to replace in
+ * @param id The identifier to replace
+ * @param replacement The replacement
+ * @returns The replaced string
+ */
+export function replaceAndCheckById (string: string, id: string, replacement: string): string {
+  // /(?<before>id="projectName"[^>]+>)(?<content>[^<]+)(?<after><\/)/gu
+  const regex = new RegExp(`(?<before>id="${id}"[^>]+>)(?<content>[^<]+)(?<after><\/)`, 'gu')
+  return replaceAndCheck(string, regex, replacement)
+}
