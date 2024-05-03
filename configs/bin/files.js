668f4b4a45b8bbb1f4cf4e1ba02014d9d06d8cf9
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-05-03 17:13:29 +0200

    fix greenshot cfg

--- configs/bin/files.js
+++ configs/bin/files.js
@@ -40,7 +40,7 @@ const configs = [
 const windowsConfigs = [
   { source: `${home}/.bashrc`, renameTo: '.bashrc-windows' },
   { source: `${appData}/Launchy/launchy.ini`, removeLinesAfter: /\[History\]/u, removeLinesMatching: [/^(?:pos=|proxyType=)/u] },
-  { source: `${appData}/Greenshot/Greenshot.ini`, removeLinesAfter: /\[Editor\]/u, removeLinesMatching: [/^(?:BaseIconSize|ImgurUploadHistory|LastCapturedRegion|LastSaveWithVersion|LastUpdateCheck|OutputFileAsFull|OutputFilePath|Win10BorderCrop|Commands=)/u, /MS Paint/u] },
+  { source: `${appData}/Greenshot/Greenshot.ini`, removeLinesAfter: /PowerpointSlideLayout=ppLayoutPictureWithCaption/u, removeLinesMatching: [/^(?:BaseIconSize|ImgurUploadHistory|LastCapturedRegion|LastSaveWithVersion|LastUpdateCheck|OutputFileAsFull|OutputFilePath|Win10BorderCrop|Commands=)/u, /MS Paint/u] },
 ]
 
 const linuxConfigs = [
