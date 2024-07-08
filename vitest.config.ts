fd9c81d06b8832c89c65e49175add7c87817aab7
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-07-08 15:28:03 +0200

    bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vitest/config'
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default defineConfig({
   test: {
     coverage: {
-      exclude: ['**/*.types.ts', 'tests/utils.ts'],
+      exclude: ['**/*.types.ts', 'tests/utils.ts', '*.config.*'],
       reporter: ['text', 'lcov', 'html'],
       thresholds: {
         100: true,
