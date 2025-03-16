92f025c2c217c1051b35c8021a6b57894680dbf0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-03-16 12:17:13 +0100

    chore: add notification sound and update script execution in clean-trackers.bat

--- one-file/clean-trackers.bat
+++ one-file/clean-trackers.bat
@@ -1,3 +1,6 @@
 @echo off
 
-node "%USERPROFILE%\Projects\github\snippets\one-file\clean-trackers.cli.js" > "%USERPROFILE%\Projects\github\snippets\one-file\clean-trackers.log"
+powershell -c "(New-Object Media.SoundPlayer 'windows-xp-notify.wav').PlaySync();"
+
+bun "%USERPROFILE%\Projects\github\snippets\one-file\clean-trackers.cli.js" > "%USERPROFILE%\Projects\github\snippets\one-file\clean-trackers.log"
+
