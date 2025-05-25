5f0dbe44db38d37d30659d6b340fcdf398b442a8
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-05-25 15:06:20 +0200

    refactor: update test to avoid useless logs

--- one-file/repo-banner.test.ts
+++ one-file/repo-banner.test.ts
@@ -1,37 +1,42 @@
-import { expect, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { svgMockA } from './repo-banner.mock'
 import { extractData, logger, replaceAndCheck, replaceAndCheckById, safeRead } from './repo-banner.utils'
 
-it('replaceAndCheck A', () => {
-  expect(replaceAndCheck('Hello world !', /(?<before>Hello )world(?<after> !)/gu, 'there')).toMatchInlineSnapshot('"Hello there !"')
-})
+describe('repo-banner', () => {
+  beforeAll(() => {
+    logger.disable()
+  })
 
-logger.info('Dont worry, the following error is expected')
+  afterAll(() => {
+    logger.enable()
+  })
 
-it('replaceAndCheck B generate error', () => {
-  expect(replaceAndCheck('Hello world !', /(?<before>Hello )world(?<after> !)/gu, 'world')).toMatchInlineSnapshot('"Hello world !"')
-})
+  it('replaceAndCheck A', () => {
+    expect(replaceAndCheck('Hello world !', /(?<before>Hello )world(?<after> !)/gu, 'there')).toMatchInlineSnapshot('"Hello there !"')
+  })
 
-logger.info('That was it, are you good ?')
+  it('replaceAndCheck B generate error', () => {
+    expect(replaceAndCheck('Hello world !', /(?<before>Hello )world(?<after> !)/gu, 'world')).toMatchInlineSnapshot('"Hello world !"')
+  })
 
-it('replaceAndCheckById A', () => {
-  expect(replaceAndCheckById(svgMockA, 'projectName', 'My project name')).toMatchSnapshot()
-})
+  it('replaceAndCheckById A', () => {
+    expect(replaceAndCheckById(svgMockA, 'projectName', 'My project name')).toMatchSnapshot()
+  })
 
-it('safeRead A non-existing file', () => {
-  expect(safeRead('plop.txt')).toBe('')
-})
+  it('safeRead A non-existing file', () => {
+    expect(safeRead('plop.txt')).toBe('')
+  })
 
-it('safeRead B existing file', () => {
-  expect(safeRead('README.md')).toContain('Snippets')
-})
+  it('safeRead B existing file', () => {
+    expect(safeRead('README.md')).toContain('Snippets')
+  })
 
-it('safeRead C existing directory', () => {
-  expect(safeRead('.')).toBe('')
-})
+  it('safeRead C existing directory', () => {
+    expect(safeRead('.')).toBe('')
+  })
 
-it('extractData A current folder', () => {
-  expect(extractData()).toMatchInlineSnapshot(`
+  it('extractData A current folder', () => {
+    expect(extractData()).toMatchInlineSnapshot(`
     {
       "color": "#54143A",
       "description": "My personal snippets",
@@ -39,10 +44,10 @@ it('extractData A current folder', () => {
       "scope": "Shuunen",
     }
   `)
-})
+  })
 
-it('extractData B another folder', () => {
-  expect(extractData('..')).toMatchInlineSnapshot(`
+  it('extractData B another folder', () => {
+    expect(extractData('..')).toMatchInlineSnapshot(`
     {
       "color": "#024eb8",
       "description": "A placeholder description",
@@ -50,4 +55,5 @@ it('extractData B another folder', () => {
       "scope": "JohnDoe",
     }
   `)
+  })
 })
